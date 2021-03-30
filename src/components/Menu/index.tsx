63bb7da350c43b9387bc53653f3009026819dc5c
Jhonny <jhonny-cwb@hotmail.com>
2021-03-30 14:03:26 -0300

    feat: menu navigation

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,4 +1,6 @@
-import { FC, useCallback, useEffect } from 'react'
+import { FC, useCallback, useEffect, useState } from 'react'
+import { useGlobalContext } from '../../contexts/global'
+import { useHistory } from 'react-router-dom'
 
 import Checkbox from '../Checkbox'
 
@@ -6,9 +8,12 @@ import './styles.scss'
 
 import { ReactComponent as HomeIcon } from '../../assets/icons/home.svg'
 import { ReactComponent as StarIcon } from '../../assets/icons/star.svg'
-import { ReactComponent as HistoryIcon } from '../../assets/icons/history.svg'
 
 const Menu: FC<Navegateble> = ({ isFocused, onFocusRight }) => {
+	const [focusedIndex, setFocusedIndex] = useState<number>(1)
+	const { toggleDarkMode } = useGlobalContext()
+	const { push } = useHistory()
+
 	const handleKeyDown = useCallback(
 		(event: KeyboardEvent) => {
 			if (!isFocused) {
@@ -21,11 +26,37 @@ const Menu: FC<Navegateble> = ({ isFocused, onFocusRight }) => {
 						onFocusRight()
 					}
 					break
+				case 'ArrowDown':
+					if (focusedIndex !== 4) {
+						setFocusedIndex(focusedIndex + 1)
+					}
+					break
+				case 'ArrowUp':
+					if (focusedIndex !== 1) {
+						setFocusedIndex(focusedIndex - 1)
+					}
+					break
+				case 'Enter':
+				case 'NumpadEnter':
+					switch (focusedIndex) {
+						case 1:
+							push('/')
+							break
+						case 2:
+							push('/favorites')
+							break
+						case 3:
+							toggleDarkMode()
+							break
+						default:
+							break
+					}
+					break
 				default:
 					break
 			}
 		},
-		[isFocused, onFocusRight]
+		[focusedIndex, isFocused, onFocusRight, push, toggleDarkMode]
 	)
 
 	useEffect(() => {
@@ -37,22 +68,19 @@ const Menu: FC<Navegateble> = ({ isFocused, onFocusRight }) => {
 		<>
 			{isFocused && <div className="menu__background" />}
 			<div className={`menu ${!isFocused && 'menu--hidden'}`}>
-				<div className="menu__item">
-					<HomeIcon height={25} width={25} className="menu__icon" />
-					Início
+				<div className="menu__links">
+					<div className={`menu__item ${focusedIndex === 1 && 'menu__item--focused'}`}>
+						<HomeIcon height={25} width={25} className="menu__icon" />
+						Início
+					</div>
+
+					<div className={`menu__item ${focusedIndex === 2 && 'menu__item--focused'}`}>
+						<StarIcon height={25} width={25} className="menu__icon" />
+						Favoritos
+					</div>
 				</div>
 
-				<div className="menu__item">
-					<StarIcon height={25} width={25} className="menu__icon" />
-					Favoritos
-				</div>
-
-				<div className="menu__item">
-					<HistoryIcon height={25} width={25} className="menu__icon" />
-					Favoritos
-				</div>
-
-				<div className="menu__footer">
+				<div className={`menu__item ${focusedIndex === 3 && 'menu__item--focused'}`}>
 					Modo escuro
 					<Checkbox />
 				</div>
