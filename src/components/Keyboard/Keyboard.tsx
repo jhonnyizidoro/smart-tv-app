85c86ae02cfbda9e62800f35e3f265eddfd2be64
Jhonny <jhonny-cwb@hotmail.com>
2021-04-05 18:48:31 -0300

    fix: removed unused key

--- src/components/Keyboard/Keyboard.tsx
+++ src/components/Keyboard/Keyboard.tsx
@@ -19,7 +19,7 @@ const Keyboard: FC<KeyboardProps> = ({ onKeyPress, onFocusLeft, onFocusDown }) =
 			['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'backspace'],
 			['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
 			['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'enter'],
-			['z', 'x', 'c', 'v', 'b', 'n', 'm', 'sair'],
+			['z', 'x', 'c', 'v', 'b', 'n', 'm'],
 		],
 		[]
 	)
